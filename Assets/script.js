08b1f70aa002eea9f2c0b9a5635f19ccb14d8c7a
KevinPXu <kevinpxu21@gmail.com>
2022-10-09 15:53:53 -0500

    fixed getApiData function

--- Assets/script.js
+++ Assets/script.js
@@ -1,35 +1,38 @@
 var userFormEl = $(".user-form");
 var locationInputEl = $("#location");
-var customApiURL = "";
-var location = "";
-var coordinates = [];
-
+var cityName = "";
 function formSubmitHandler(event) {
   event.preventDefault();
   console.log();
-  location = locationInputEl.val().trim();
-  console.log(location);
-  if (location) {
-    getApiKey(location);
+  cityName = locationInputEl.val().trim();
+  console.log(cityName);
+  if (cityName) {
+    getApiData(cityName);
     locationInputEl = " ";
   } else {
     alert("Please enter a location");
   }
 }
 
-function getCoordinates(location) {
-  var apiUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    location +
-    "&limit=1&appid=baa49ab2f93c267a38ffa79b5f8e1d8d";
-  fetch(apiUrl).then(function (response) {
-    return response.json();
-  });
+async function getApiData(cityName) {
+  var weatherApiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=baa49ab2f93c267a38ffa79b5f8e1d8d&units=imperial";
+
+  console.log(weatherApiUrl);
+
+  var res = await fetch(weatherApiUrl);
+  var data = await res.json();
+  console.log(data);
+  todaysForecast(data);
+  fiveDayForecast(data);
 }
 
-function getApiKey(location) {
-  var coords = getCoordinates(location);
-  console.log(coords);
+function fiveDayForecast(data) {
+  var date = "";
+  for (var i = 8; i <= 40; i + 8) {
+    date = new Date(data.list[i].dt * 1000);
+  }
 }
-
 userFormEl.on("submit", formSubmitHandler);
